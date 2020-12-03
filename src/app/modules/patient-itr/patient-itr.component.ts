894e45b851c4237094822c452a4f4ec57e32ef07
chinchan37 <christian.santos37@gmail.com>
2020-12-03 22:28:46 +0800

    update on medical journal, add list:

--- src/app/modules/patient-itr/patient-itr.component.ts
+++ src/app/modules/patient-itr/patient-itr.component.ts
@@ -6,6 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-itr.component.scss']
 })
 export class PatientItrComponent implements OnInit {
+  medical_journal = [
+    {
+      visit_date: "July 01, 2020",
+      visits: [
+        {
+          visit_type: "GENERAL CONSULTATION",
+          assessed_by: "John Doe",
+          diagnosed_by: "Dr. Moira Santos"
+        },
+        {
+          visit_type: "LABORATORY - URINALYSIS",
+          assessed_by: "John Doe",
+          diagnosed_by: "Dr. Francis Gamboa"
+        }
+      ]
+    },
+    {
+      visit_date: "March 09, 2020",
+      visits: [
+        {
+          visit_type: "FAMILY PLANNING",
+          assessed_by: "Midwife 1",
+          diagnosed_by: "Nurse 1"
+        }
+      ]
+    },
+    {
+      visit_date: "March 01, 2020",
+      visits: [
+        {
+          visit_type: "FAMILY PLANNING",
+          assessed_by: "Midwife 1",
+          diagnosed_by: "Nurse 1"
+        }
+      ]
+    },
+    {
+      visit_date: "Feb 14, 2020",
+      visits: [
+        {
+          visit_type: "GENERAL CONSULTATION",
+          assessed_by: "Procorpio Pepito",
+          diagnosed_by: "Pepito Sampu"
+        }
+      ]
+    }
+  ];
 
   constructor() { }
 
