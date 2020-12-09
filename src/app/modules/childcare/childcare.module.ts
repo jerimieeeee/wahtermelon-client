d76360f6d3625d0633821c2d0b3ac78ff0714e5b
Francis Fernandez <franzfernandez163@gmail.com>
2020-12-09 13:20:26 +0800

    child care update

--- src/app/modules/childcare/childcare.module.ts
+++ src/app/modules/childcare/childcare.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ChildcareRoutingModule } from './childcare-routing.module';
 import { ChildcareComponent } from './childcare.component';
 import { SharedComponentsModule } from 'src/app/shared/shared-components/shared-components.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 
 @NgModule({
   declarations: [
-    ChildcareComponent,
-  ],
+    ChildcareComponent],
   imports: [
     CommonModule,
     ChildcareRoutingModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    FontAwesomeModule
   ]
 })
 export class ChildcareModule { }
