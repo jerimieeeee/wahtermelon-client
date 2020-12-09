a288d2ba6652def4642053bc391c2643fe2a3940
Francis Fernandez <franzfernandez163@gmail.com>
2020-12-09 13:19:19 +0800

    added variable for the services of cc module

--- src/app/modules/childcare/childcare.component.ts
+++ src/app/modules/childcare/childcare.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch,faBalanceScale,faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-childcare',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildcareComponent implements OnInit {
 
+  faSearch = faSearch;
+  faBalanceScale = faBalanceScale;
+  faPlus = faPlus;
+
+  services = [
+    {
+      id: "cc",
+      desc: "cord clamping"
+    },
+    {
+      id: "drying",
+      desc: "drying"
+    },
+    {
+      id: "bf",
+      desc: "breastfeeding"
+    }
+  ]
+  
+  showModal = false;
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
+
+  showServiceModal = false;
+  toggleServiceModal(){
+    this.showServiceModal = !this.showServiceModal;
+  }
+
+  showVaccineModal = false;
+  toggleVaccineModal(){
+    this.showVaccineModal = !this.showVaccineModal;
+  }
+  
+  showBreastfeedingModal = false;
+  toggleBreastfeedingModal(){
+    this.showBreastfeedingModal = !this.showBreastfeedingModal;
+  }
+
+  
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
