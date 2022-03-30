508fc27e799708a1b31fb666f39fe2e952bf0f6a
marps <rdnmarps03@gmail.com>
2022-03-30 21:26:57 +0800

    working2.0 03-30-2022

--- src/app/modules/familyplanning/fphx/fphx.component.ts
+++ src/app/modules/familyplanning/fphx/fphx.component.ts
@@ -43,41 +43,50 @@ export class FphxComponent implements OnInit {
   public heent_strings = [];
   public skin_strings = [];
   public fphx_strings = [];
+  public cat_strings = [];
+
+  public check_list = [];
 
   public module_strings = [];
   public fp_hx_symptoms = [
   {"history_id":"ALLERGY","history_text":"Allergies","history_cat":"ANY","x":0 },
   {"history_id":"ANEMIA","history_text":"Anemia","history_cat":"ANY","x":0 },
   {"history_id":"BLEEDING","history_text":"Bleeding tendencies (nose, gums, etc.)","history_cat":"ANY","x":0 },
-  {"history_id":"BRSTMASS","history_text":"Breast\/axillary masses","history_cat":"CXHRT","x":0 },
-  {"history_id":"CVAHARHD","history_text":"Family history of CVA (strokes), hypertension, asthma, rheumatic heart disease","history_cat":"CXHRT","x":0 },
-  {"history_id":"CXPAIN","history_text":"Severe chest pain","history_cat":"CXHRT","x":0 },
   {"history_id":"DIABETES","history_text":"Diabetes","history_cat":"ANY","x":0 },
-  {"history_id":"DIAS90","history_text":"Diastolic of 90 & above","history_cat":"CXHRT","x":0 },
   {"history_id":"DRUGINTAKE","history_text":"Drug intake (anti-TB, anti-diabetic, anticonvulsant)","history_cat":"ANY","x":0 },
   {"history_id":"ECTPREG","history_text":"Ectopic pregnancy","history_cat":"ANY","x":0 },
-  {"history_id":"EPILEPSY","history_text":"Epilepsy\/Convulsion\/Seizure","history_cat":"HEENT","x":0 },
-  {"history_id":"ETHY","history_text":"Enlarged thyroid","history_cat":"HEENT","x":0 },
-  {"history_id":"FATIGUE","history_text":"Shortness of breath and easy fatiguability","history_cat":"CXHRT","x":0 },
-  {"history_id":"GALL","history_text":"History of gallbladder disease","history_cat":"ABD","x":0 },
-  {"history_id":"HEADACHE","history_text":"Severe headache\/dizziness","history_cat":"HEENT","x":0 },
-  {"history_id":"HMOLE","history_text":"Hydatidiform mole (w\/in the last 12 mos.)","history_cat":"ANY","x":0 },
-  {"history_id":"INTERBLEED","history_text":"Intermenstrual bleeding","history_cat":"GEN","x":0 },
-  {"history_id":"LEGPAIN","history_text":"Swelling or severe pain in the legs not related to injuries","history_cat":"EXT","x":0 },
-  {"history_id":"LIVER","history_text":"History of liver disease","history_cat":"ABD","x":0 },
-  {"history_id":"MASSABD","history_text":"Mass in the abdomen","history_cat":"ABD","x":0 },
+  {"history_id":"HMOLE","history_text":"Hydatidiform mole (w/in the last 12 mos.)","history_cat":"ANY","x":0 },
   {"history_id":"MPARTNERS","history_text":"Multiple partners","history_cat":"ANY","x":0 },
-  {"history_id":"NIPBLOOD","history_text":"Nipple discharges (blood)","history_cat":"CXHRT","x":0 },
-  {"history_id":"NIPPUS","history_text":"Nipple discharges (pus)","history_cat":"CXHRT","x":0 },
-  {"history_id":"POSTBLEED","history_text":"Postcoital bleeding","history_cat":"GEN","x":0 },
   {"history_id":"SMOKING","history_text":"Smoking","history_cat":"ANY","x":0 },
   {"history_id":"STD","history_text":"STD","history_cat":"ANY","x":0 },
+
+  {"history_id":"BRSTMASS","history_text":"Breast/axillary masses","history_cat":"CXHRT","x":0 },
+  {"history_id":"CVAHARHD","history_text":"Family history of CVA (strokes), hypertension, asthma, rheumatic heart disease","history_cat":"CXHRT","x":0 },
+  {"history_id":"CXPAIN","history_text":"Severe chest pain","history_cat":"CXHRT","x":0 },
+  {"history_id":"DIAS90","history_text":"Diastolic of 90 & above","history_cat":"CXHRT","x":0 },
+  {"history_id":"FATIGUE","history_text":"Shortness of breath and easy fatiguability","history_cat":"CXHRT","x":0 },
+  {"history_id":"NIPBLOOD","history_text":"Nipple discharges (blood)","history_cat":"CXHRT","x":0 },
+  {"history_id":"NIPPUS","history_text":"Nipple discharges (pus)","history_cat":"CXHRT","x":0 },
   {"history_id":"SYS140","history_text":"Systolic of 140 & above","history_cat":"CXHRT","x":0 },
+
+  {"history_id":"EPILEPSY","history_text":"Epilepsy/Convulsion/Seizure","history_cat":"HEENT","x":0 },
+  {"history_id":"ETHY","history_text":"Enlarged thyroid","history_cat":"HEENT","x":0 },
+  {"history_id":"HEADACHE","history_text":"Severe headache/dizziness","history_cat":"HEENT","x":0 },
+  {"history_id":"VISION","history_text":"Visual disturbance/blurring of vision","history_cat":"HEENT","x":0 },
+  {"history_id":"YCONJ","history_text":"Yellowish conjunctive","history_cat":"HEENT","x":0 },
+
+  {"history_id":"GALL","history_text":"History of gallbladder disease","history_cat":"ABD","x":0 },
+  {"history_id":"LIVER","history_text":"History of liver disease","history_cat":"ABD","x":0 },
+  {"history_id":"MASSABD","history_text":"Mass in the abdomen","history_cat":"ABD","x":0 },
+
+  {"history_id":"INTERBLEED","history_text":"Intermenstrual bleeding","history_cat":"GEN","x":0 },
+  {"history_id":"POSTBLEED","history_text":"Postcoital bleeding","history_cat":"GEN","x":0 },
   {"history_id":"UTERUS","history_text":"Mass in the uterus","history_cat":"GEN","x":0 },
   {"history_id":"VAGDISCH","history_text":"Vaginal discharge","history_cat":"GEN","x":0 },
+
+  {"history_id":"LEGPAIN","history_text":"Swelling or severe pain in the legs not related to injuries","history_cat":"EXT","x":0 },
   {"history_id":"VARICOSE","history_text":"Severe varicosities","history_cat":"EXT","x":0 },
-  {"history_id":"VISION","history_text":"Visual disturbance\/blurring of vision","history_cat":"HEENT","x":0 },
-  {"history_id":"YCONJ","history_text":"Yellowish conjunctive","history_cat":"HEENT","x":0 },
+
   {"history_id":"YELLOWSKIN","history_text":"Yellowish skin","history_cat":"SKIN","x":0 }];
 
   public fp_hx_cat = [];
@@ -204,121 +213,157 @@ flip(): void{
 }
 
 uncheck(cancel_name){
- if(cancel_name == 'cancelAbdomen'){
-  this.abdomen_typing = false;
-  this.abdomen_strings = [];
-  console.log(this.abdomen_strings + ' emptying out');
-}else if(cancel_name == 'cancelHistory'){
-  this.history_typing = false;
-  this.history_strings = [];
-  console.log(this.history_strings + ' emptying out');
-}else if(cancel_name == 'cancelHeart'){
-  this.heart_typing = false;
-  this.heart_strings = [];
-  console.log(this.heart_strings + ' emptying out');
-}else if(cancel_name == 'cancelExtreme'){
-  this.extremities_typing = false;
-  this.extremities_strings = [];
-  console.log(this.extremities_strings + ' emptying out');
-}else if(cancel_name == 'cancelGenital'){
-  this.genital_typing = false;
-  this.genital_strings = [];
-  console.log(this.genital_strings + ' emptying out');
-}else if(cancel_name == 'cancelHEENT'){
-  this.heent_typing = false;
-  this.heent_strings = [];
-  console.log(this.heent_strings + ' emptying out');
-}else if(cancel_name == 'cancelSkin'){
-  this.skin_typing = false;
-  this.skin_strings = [];
-  console.log(this.skin_strings + ' emptying out');
-}else if(cancel_name == 'cancelFPHX'){
-  this.fphx_typing = false;
-  this.fphx_strings = [];
-  console.log(this.fphx_strings + ' emptying out');
-}
+
+  let cat = cancel_name.replace("cancel",'');
+  console.log(cat + ' this is my cat from uncheck');
+  this.cat_strings.splice(this.fphx_strings.indexOf(cat),1 );
+  console.log("emtying out cat_strings");
+  
+  this.fp_hx_symptoms.forEach( e => {
+    if(e.history_cat == cat){
+      if(this.fphx_strings.includes(e.history_text)){
+        console.log(this.fphx_strings);
+        
+        this.fphx_strings.splice(this.fphx_strings.indexOf(e.history_text),1 );
+      }
+     
+    }
+  });
+//  if(cancel_name == 'cancelABD'){
+//   this.abdomen_typing = false;
+//   this.abdomen_strings = [];
+//   console.log(this.abdomen_strings + ' emptying out');
+// }else if(cancel_name == 'cancelANY'){
+//   this.history_typing = false;
+//   this.history_strings = [];
+//   console.log(this.history_strings + ' emptying out');
+// }else if(cancel_name == 'cancelCXHRT'){
+//   this.heart_typing = false;
+//   this.heart_strings = [];
+//   console.log(this.heart_strings + ' emptying out');
+// }else if(cancel_name == 'cancelEXT'){
+//   this.extremities_typing = false;
+//   this.extremities_strings = [];
+//   console.log(this.extremities_strings + ' emptying out');
+// }else if(cancel_name == 'cancelGEN'){
+//   this.genital_typing = false;
+//   this.genital_strings = [];
+//   console.log(this.genital_strings + ' emptying out');
+// }else if(cancel_name == 'cancelHEENT'){
+//   this.heent_typing = false;
+//   this.heent_strings = [];
+//   console.log(this.heent_strings + ' emptying out');
+// }else if(cancel_name == 'cancelSKIN'){
+//   this.skin_typing = false;
+//   this.skin_strings = [];
+//   console.log(this.skin_strings + ' emptying out');
+// }
+// else if(cancel_name == 'cancelANY'){
+//   this.fphx_typing = false;
+//   this.fphx_strings = [];
+//   console.log(this.fphx_strings + ' emptying out');
+// }
 }
 
-onChange(id_name){  
+onChange(id_name,cat){  
   
-  this.abdomen_symptoms.forEach(abdomenElement => {
-    if(abdomenElement.name == id_name){
-    this.abdomen_typing = true;
-    this.abdomen_strings.push(id_name);
-    console.log(this.abdomen_strings);
+  // this.abdomen_symptoms.forEach(abdomenElement => {
+  //   if(abdomenElement.name == id_name){
+  //   this.abdomen_typing = true;
+  //   this.abdomen_strings.push(id_name);
+  //   console.log(this.abdomen_strings);
+  // }
+  // });
+  // this.history_symptoms.forEach(historyElement => {
+  //   if(historyElement.name == id_name){
+  //   this.history_typing = true;
+  //   this.history_strings.push(id_name);
+  //   console.log(this.history_strings);
+  // }
+  // });
+  // this.heart_symptoms.forEach(heartElement => {
+  //   if(heartElement.name == id_name){
+  //   this.heart_typing = true;
+  //   this.heart_strings.push(id_name);
+  //   console.log(this.heart_strings);
+  // }
+  // });
+  // this.extremities_symptoms.forEach(extremeElement => {
+  //   if(extremeElement.name == id_name){
+  //   this.extremities_typing = true;
+  //   this.extremities_strings.push(id_name);
+  //   console.log(this.extremities_strings);
+  // }
+  // });
+  // this.genital_symptoms.forEach(genitalElement => {
+  //   if(genitalElement.name == id_name){
+  //   this.genital_typing = true;
+  //   this.genital_strings.push(id_name);
+  //   console.log(this.genital_strings);
+  // }
+  // });
+  // this.heent_symptoms.forEach(heentElement => {
+  //   if(heentElement.name == id_name){
+  //   this.heent_typing = true;
+  //   this.heent_strings.push(id_name);
+  //   console.log(this.heent_strings);
+  // }
+  // });
+  // this.skin_symptoms.forEach(skinElement => {
+  //   if(skinElement.name == id_name){
+  //   this.skin_typing = true;
+  //   this.skin_strings.push(id_name);
+  //   console.log(this.skin_strings);
+  // }
+  // });
+  // console.log(id_name + ' onchange CHECKKSKSKSKSKSKSK adn this is my cat ngeow  ?>>>> ' + cat);
+  if(!this.cat_strings.includes(cat)){
+    this.cat_strings.push(cat);
   }
-  });
-  this.history_symptoms.forEach(historyElement => {
-    if(historyElement.name == id_name){
-    this.history_typing = true;
-    this.history_strings.push(id_name);
-    console.log(this.history_strings);
-  }
-  });
-  this.heart_symptoms.forEach(heartElement => {
-    if(heartElement.name == id_name){
-    this.heart_typing = true;
-    this.heart_strings.push(id_name);
-    console.log(this.heart_strings);
-  }
-  });
-  this.extremities_symptoms.forEach(extremeElement => {
-    if(extremeElement.name == id_name){
-    this.extremities_typing = true;
-    this.extremities_strings.push(id_name);
-    console.log(this.extremities_strings);
-  }
-  });
-  this.genital_symptoms.forEach(genitalElement => {
-    if(genitalElement.name == id_name){
-    this.genital_typing = true;
-    this.genital_strings.push(id_name);
-    console.log(this.genital_strings);
-  }
-  });
-  this.heent_symptoms.forEach(heentElement => {
-    if(heentElement.name == id_name){
-    this.heent_typing = true;
-    this.heent_strings.push(id_name);
-    console.log(this.heent_strings);
-  }
-  });
-  this.skin_symptoms.forEach(skinElement => {
-    if(skinElement.name == id_name){
-    this.skin_typing = true;
-    this.skin_strings.push(id_name);
-    console.log(this.skin_strings);
-  }
-  });
-  console.log(id_name);
+  
+  console.log(this.cat_strings);
   
   this.fp_hx_symptoms.forEach(fphxElement => {
     if(fphxElement.history_text == id_name){
+     
     this.fphx_typing = true;
-    this.fphx_strings.push(id_name);
-    console.log(this.fphx_strings);
+      if(this.fphx_strings.includes(id_name)){
+        // console.log("existing will not push");
+      }else{
+        this.fphx_strings.push(id_name);
+      }
+    // console.log(this.fphx_strings);
   }
   });
   
 }
 
 showModule(id_name){  
-  console.log(id_name);
+  this.module_strings = [];
+  this.check_list = [];
+  // console.log(id_name);
   this.module_strings.push(id_name);
-  console.log(this.module_strings);
+  // console.log(this.module_strings);
+  this.fp_hx_symptoms.forEach(element => {
+    if(element.history_cat == id_name){
+      // console.log(element.history_text);
+      this.check_list.push(element.history_text);
+      // console.log(this.check_list);
+    }
+  });
 }
 
 hideModule(id_name){  
   if(this.module_strings.includes(id_name)){
-    console.log(id_name);
+    // console.log(id_name);
     this.module_strings.splice(this.module_strings.indexOf(id_name),1 );
   }
-  console.log(this.module_strings);
+  // console.log(this.module_strings);
   
 //  this.typing = true;
 }
 logConsole(id_name){
-console.log(id_name);
+// console.log(id_name);
 
 }
 
@@ -327,7 +372,7 @@ checkArrayFam(id_name){
 }
 
 logForText(text){
- console.log(text + " this is my log text");
+//  console.log(text + " this is my log text");
  
 }
 }
