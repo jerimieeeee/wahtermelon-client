1eec4a8fff7547b8ac80d8a232a126347cf51e7d
marps <rdnmarps03@gmail.com>
2022-05-24 17:04:19 +0800

    added more to postpartum and done prenatal

--- src/app/modules/maternalcare/maternalcare.component.ts
+++ src/app/modules/maternalcare/maternalcare.component.ts
@@ -10,13 +10,12 @@ export class MaternalcareComponent implements OnInit {
   constructor() { }
   module: number;
   ngOnInit(): void {
-    this.module = 1;
+    this.module = 2;
   }
 
   switchTab(tab) {
     this.module = 0;
     this.module = tab;
     console.log(this.module);
-    
   }
 }
--- src/app/modules/maternalcare/modules/postpartum/postpartum.component.ts
+++ src/app/modules/maternalcare/modules/postpartum/postpartum.component.ts
@@ -9,20 +9,94 @@ import { faCalendarDay, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class PostpartumComponent implements OnInit {
   focused: boolean;
-  keyUp: any[];
-  buttons: any[];
+  public keyUp = [];
+  public buttons = [];
   faInfoCircle = faInfoCircle;
   faCalendarDay = faCalendarDay;
+  postpartum_form: FormGroup;
+  gravidity: Number = new Number();
+  parity: Number = new Number();
+  full_term: Number = new Number();
+  live_births: Number = new Number();
+  abortions: Number = new Number();
+  pre_term: Number = new Number();
+  bfd: boolean;
+
   constructor() { }
 
+  public delivery_location = [
+    {id:'1', loc:'Home'},
+    {id:'2', loc:'Public Hospital'},
+    {id:'3', loc:'Private Hospital'},
+    {id:'4', loc:'Public Lying-In Clinic'},
+    {id:'5', loc:'Private Lying-In Clinic'},
+    {id:'6', loc:'Health Center'},
+    {id:'7', loc:'Barangay Health Station'},
+    {id:'8', loc:'DOH-Licensed Ambulance'},
+    {id:'9', loc:'Others'},
+  ];
+  public attendants = [
+    {id:'1', attendant:'Physician'},
+    {id:'2', attendant:'Midwife'},
+    {id:'3', attendant:'Nurse'},
+    {id:'4', attendant:'Trained Hilot'},
+    {id:'5', attendant:'Untrained Hilot'},
+    {id:'6', attendant:'Other'},
+  ];
+  public preg_outcome = [
+    {id:'1', outcome:'Fetal Death in Utero - Male'},
+    {id:'2', outcome:'Fetal Death in Utero - Male'},
+    {id:'3', outcome:'Live baby girl LSCS'},
+    {id:'4', outcome:'Live baby boy LSCS'},
+    {id:'5', outcome:'Live baby girl NSD'},
+    {id:'6', outcome:'Live baby boy NSD'},
+    {id:'7', outcome:'Stillbirth - Male'},
+    {id:'8', outcome:'Stillbirth - Female'},
+    {id:'9', outcome:'Twin'},
+  ]
   ngOnInit(): void {
-    this.focused = true;
+   this.createForm();
   }
-
+ createForm(){
+  this.postpartum_form = new FormGroup({
+    registration_date: new FormControl(new Date().toISOString().substring(0,10)),
+    delivery_date: new FormControl(new Date().toISOString().substring(0,10)),
+    admission_date: new FormControl(new Date().toISOString().substring(0,10)),
+    discharge_date: new FormControl(new Date().toISOString().substring(0,10)),
+    gravidity: new FormControl(this.gravidity),
+    parity: new FormControl(this.parity),
+    full_term: new FormControl(this.full_term),
+    pre_term: new FormControl(this.pre_term),
+    abortions: new FormControl(this.abortions),
+    live_births: new FormControl(this.live_births),
+    delivery_location: new FormControl(),
+    region: new FormControl(),
+    province: new FormControl(),
+    muncity: new FormControl(),
+    barangay: new FormControl(),
+    birth_attendant: new FormControl(),
+    pregnancy_outcome: new FormControl(),
+    breastfeeding_asap: new FormControl(),
+    baby_healthy: new FormControl(),
+    breastfeeding_date: new FormControl(new Date().toISOString().substring(0,10)),
+  });
+ }
   flip(): void{
     this.focused = !this.focused;
     this.keyUp = [];
     this.buttons = [];
     this.buttons.push('save');
   }
+ saveForm(data){
+  this.postpartum_form.setValue({
+    delivery_location: data.delivery,
+    // region:  ,
+    // province: ,
+    // muncity: ,
+    // barangay: 
+  })
+ }
+ bfdCheck(){
+  this.bfd = !this.bfd;
+ }
 }
