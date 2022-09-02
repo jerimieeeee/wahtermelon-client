912f877a0da6fcb8a381182ef73286399a05b14d
vicballs <vinantonioph@gmail.com>
2022-09-02 17:06:40 +0800

    update ncd template

--- src/app/modules/ncd/ncd.component.ts
+++ src/app/modules/ncd/ncd.component.ts
@@ -8,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class NcdComponent implements OnInit {
 
   module: Number;
+  modules: Number;
   constructor() { }
 
   ngOnInit(): void {
     this.module=1;
+    this.modules=1;
   }
 
   switchTab(tab){
     this.module = 0;
     this.module = tab;
+  }
+
+  switchTabs(tabs){
+    this.modules = 0;
+    this.modules = tabs;
   }
 }
