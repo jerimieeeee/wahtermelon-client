74b9abc623b0fd4a0e43c6ea5d3e8f69ba8de7d9
vicballs <vinantonioph@gmail.com>
2022-09-08 19:17:02 +0800

    update drug list modals

--- src/app/modules/drug-list/components/drugs/drugs.component.ts
+++ src/app/modules/drug-list/components/drugs/drugs.component.ts
@@ -50,6 +50,39 @@ export class DrugsComponent implements OnInit {
     this.is_searching4= true;
   }
 
+  lib_drugs = [
+    {code: '01', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '02', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '03', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '04', bname: 'Biogeic', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '05', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '06', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '07', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '08', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '09', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '10', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '12', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '13', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '14', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '15', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '16', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '17', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '18', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '19', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '20', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '21', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '22', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '23', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '24', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '25', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '26', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '27', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '28', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '29', bname: 'Cephalexin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '30', bname: 'Bio-Flu', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+    {code: '31', bname: 'Citirizin', gname:'Vitamin B', drugstr:'250mg', drugf:'Capsul', dosage:'2.00', dosageuom:'mG', dosagereg:'OD', duration:'2', frequency: 'Week'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
