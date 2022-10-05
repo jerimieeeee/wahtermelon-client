f18535e17cfa1d43ec55cd6b013392d2c7b2e0c9
Mark Santos <christian.santos37@gmail.com>
2022-10-05 15:13:50 +0800

    load patient info from url details

--- src/app/components/patient-info/patient-info.component.ts
+++ src/app/components/patient-info/patient-info.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { faFlask, faHeart, faExclamationCircle, faNotesMedical, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { AgeService } from 'app/shared/services/age.service';
+import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
   selector: 'app-patient-info',
@@ -7,6 +10,8 @@ import { faFlask, faHeart, faExclamationCircle, faNotesMedical, faPlusCircle } f
   styleUrls: ['./patient-info.component.scss']
 })
 export class PatientInfoComponent implements OnInit {
+  patient_info: any;
+
   faNotesMedical = faNotesMedical;
   faFlask = faFlask;
   faHeart = faHeart;
@@ -14,13 +19,35 @@ export class PatientInfoComponent implements OnInit {
   faPlusCircle = faPlusCircle;
 
   showModal: boolean = false;
+  show_form: boolean = false;
 
-  constructor() { }
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private http: HttpService,
+    private ageService: AgeService
+  ) {
+    this.activeRoute.params.subscribe(params => {
+      this.getPatient(params.id);
+    });
+  }
 
-  ngOnInit(): void {
+  getAge(){
+    let age_value = this.ageService.calcuateAge(this.patient_info.birthdate)
+    return age_value.age + ' ' + age_value.type+(age_value.age>1 ? 's old' : ' old' );
+  }
+
+  getPatient(id){
+    this.http.get('patient/'+id).subscribe({
+      next: (data: any) => { this.patient_info = data.data; this.show_form = true; console.log(data.data) },
+      error: err => console.log(err)
+    });
   }
 
   toggleModal(){
     this.showModal = !this.showModal;
+  }
+
+  ngOnInit(): void {
+
   }
 }
