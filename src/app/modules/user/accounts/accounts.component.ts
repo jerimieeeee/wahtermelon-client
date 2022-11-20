0997a11687ec9633802c8bfaed85fc5859c3c8e4
Mark Santos <christian.santos37@gmail.com>
2022-11-20 16:57:08 +0800

    add activation of user account

--- src/app/modules/user/accounts/accounts.component.ts
+++ src/app/modules/user/accounts/accounts.component.ts
@@ -1,4 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
@@ -7,8 +9,10 @@ import { HttpService } from 'app/shared/services/http.service';
   styleUrls: ['./accounts.component.scss']
 })
 export class AccountsComponent implements OnInit {
-
+  faSpinner = faSpinner;
   account_list: any;
+
+  is_updating: boolean = false;
 
   loadAccount(){
     this.http.get('users').subscribe({
@@ -17,6 +21,28 @@ export class AccountsComponent implements OnInit {
         this.account_list = data.data;
       },
       error: err => console.log(err)
+    })
+  }
+
+  submit_error: any;
+
+  updateActive(val, i){
+    this.is_updating = true;
+    console.log(val);
+    let params = val;
+      params.birthdate = formatDate(params.birthdate,'Y-MM-dd','en');
+      delete params.email;
+
+    this.http.update('users/', val.id, params).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.is_updating = false;
+      },
+      error: err => {
+        this.account_list[i].is_active = !params.is_active;
+        this.submit_error = err.error.errors;
+        this.is_updating = false;
+      }
     })
   }
 
