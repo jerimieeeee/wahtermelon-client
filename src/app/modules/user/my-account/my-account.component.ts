54ad1fefb64ed908e25959e27c2e910e2215e912
Mark Santos <christian.santos37@gmail.com>
2022-11-23 23:36:32 +0800

    add update of desig and facility : g

--- src/app/modules/user/my-account/my-account.component.ts
+++ src/app/modules/user/my-account/my-account.component.ts
@@ -26,6 +26,15 @@ export class MyAccountComponent implements OnInit {
   date;
   submit_errors: any;
 
+  regions: object;
+  provinces: object;
+  municipalities: object;
+  facilities: object;
+  designations: object;
+  employers: object;
+
+  orig_value: any;
+
   constructor(
     private http: HttpService,
     private formBuilder: FormBuilder,
@@ -40,7 +49,13 @@ export class MyAccountComponent implements OnInit {
     birthdate: new FormControl<string| null>(''),
     gender: new FormControl<string| null>(''),
     contact_number: new FormControl<number| null>(null),
-    photo_url: new FormControl<string| null>('')
+    photo_url: new FormControl<string| null>(''),
+    facility_code: new FormControl<string| null>(''),
+    region: new FormControl<string| null>(''),
+    province: new FormControl<string| null>(''),
+    municipality: new FormControl<string| null>(''),
+    designation_code: new FormControl<string| null>(''),
+    employer_code: new FormControl<string| null>(''),
   });
 
   enbaleEdit(){
@@ -50,8 +65,6 @@ export class MyAccountComponent implements OnInit {
       this.userForm.disable();
       this.userForm.patchValue({...this.orig_value});
     }
-
-    console.log(this.userForm);
   }
 
   onSubmit(){
@@ -98,14 +111,46 @@ export class MyAccountComponent implements OnInit {
     // this.router.navigate(['/home']);
   }
 
+  libraries = [
+    {var_name: 'suffix_names', location: 'suffix-names'},
+    {var_name: 'regions', location: 'regions'},
+    {var_name: 'designations', location: 'designations'},
+    {var_name: 'employers', location: 'employers'},
+  ]
+
   loadLibraries(){
-    this.http.get('libraries/suffix-names').subscribe({
-      next: (data: any) => {this.suffix_names = data.data},
-      error: err => console.log(err)
+    this.libraries.forEach(obj => {
+      this.http.get('libraries/'+obj.location).subscribe({
+        next: (data: any) => {this[obj.var_name] = data.data; console.log(obj.var_name, data.data)},
+        error: err => console.log(err)
+      })
     });
   }
 
-  orig_value: any;
+  loadFacilities(municipality){
+    console.log(municipality);
+    this.http.get('libraries/facilities', {params:{'filter[municipality_code]':municipality, 'per_page': 'all'}}).subscribe({
+      next: (data: any) => {
+        console.log(data)
+        this.facilities = data.data;
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  loadDemog(loc, code, include){
+    if(loc == 'regions') {
+      this.municipalities = null;
+      this.facilities = null;
+    }else if (loc == 'provinces') {
+      this.facilities = null;
+    }
+
+    this.http.get('libraries/'+loc+'/'+code,{params:{'include':include}}).subscribe({
+      next: (data: any) => {console.log(data.data); this[include] = data.data[include]},
+      error: err => console.log(err)
+    });
+  }
 
   loadUser(){
     this.http.get('users/'+localStorage.getItem('user_id')).subscribe({
@@ -114,6 +159,17 @@ export class MyAccountComponent implements OnInit {
         this.orig_value = data.data;
         this.orig_value.birthdate = formatDate(this.orig_value.birthdate,'Y-MM-dd','en')
         this.userForm.patchValue({...this.orig_value});
+
+        this.loadDemog('regions', this.orig_value.facility.region.code, 'provinces');
+        this.userForm.patchValue({region: this.orig_value.facility.region.code});
+        this.loadDemog('provinces', this.orig_value.facility.province.code, 'municipalities');
+        this.userForm.patchValue({province: this.orig_value.facility.province.code});
+        this.loadFacilities(this.orig_value.facility.municipality.code);
+        this.userForm.patchValue({municipality: this.orig_value.facility.municipality.code});
+        this.userForm.patchValue({facility_code: this.orig_value.facility.code});
+
+        this.userForm.patchValue({designation_code: this.orig_value.designation.code})
+        this.userForm.patchValue({employer_code: this.orig_value.employer.code})
 
         localStorage.setItem('user_last_name', this.orig_value.last_name);
         localStorage.setItem('user_first_name', this.orig_value.first_name);
@@ -136,7 +192,13 @@ export class MyAccountComponent implements OnInit {
       birthdate: ['', Validators.required],
       gender: ['', Validators.required],
       contact_number: ['', Validators.required],
-      photo_url: ['']
+      photo_url: [''],
+      facility_code: [''],
+      region: ['', Validators.required],
+      province: ['', Validators.required],
+      municipality: ['', Validators.required],
+      designation_code: ['', Validators.required],
+      employer_code: ['', Validators.required],
     });
 
     this.loadUser();
