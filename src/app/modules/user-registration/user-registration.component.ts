449573a517b4c8a44143658d2279f495d7ca737e
Mark Santos <christian.santos37@gmail.com>
2022-11-23 23:05:35 +0800

    add facility, designation and employer

--- src/app/modules/user-registration/user-registration.component.ts
+++ src/app/modules/user-registration/user-registration.component.ts
@@ -26,6 +26,13 @@ export class UserRegistrationComponent implements OnInit {
   date;
   submit_errors: any;
 
+  regions: object;
+  provinces: object;
+  municipalities: object;
+  facilities: object;
+  designations: object;
+  employers: object;
+
   constructor(
     private http: HttpService,
     private formBuilder: FormBuilder,
@@ -46,6 +53,12 @@ export class UserRegistrationComponent implements OnInit {
     password: new FormControl<string| null>(''),
     password_confirmation: new FormControl<string| null>(''),
     privacy: new FormControl<boolean| null>(false),
+    facility_code: new FormControl<string| null>(''),
+    region: new FormControl<string| null>(''),
+    province: new FormControl<string| null>(''),
+    municipality: new FormControl<string| null>(''),
+    designation_code: new FormControl<string| null>(''),
+    employer_code: new FormControl<string| null>(''),
   });
 
   onSubmit(){
@@ -73,6 +86,20 @@ export class UserRegistrationComponent implements OnInit {
     }
   }
 
+  loadDemog(loc, code, include){
+    if(loc == 'regions') {
+      this.municipalities = null;
+      this.facilities = null;
+    }else if (loc == 'provinces') {
+      this.facilities = null;
+    }
+
+    this.http.get('libraries/'+loc+'/'+code,{params:{'include':include}}).subscribe({
+      next: (data: any) => {console.log(data.data); this[include] = data.data[include]},
+      error: err => console.log(err)
+    });
+  }
+
   get f(): { [key: string]: AbstractControl } {
     return this.userForm.controls;
   }
@@ -85,11 +112,31 @@ export class UserRegistrationComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+  libraries = [
+    {var_name: 'suffix_names', location: 'suffix-names'},
+    {var_name: 'regions', location: 'regions'},
+    {var_name: 'designations', location: 'designations'},
+    {var_name: 'employers', location: 'employers'},
+  ]
+
   loadLibraries(){
-    this.http.get('libraries/suffix-names').subscribe({
-      next: (data: any) => {this.suffix_names = data.data},
-      error: err => console.log(err)
+    this.libraries.forEach(obj => {
+      this.http.get('libraries/'+obj.location).subscribe({
+        next: (data: any) => {this[obj.var_name] = data.data; console.log(obj.var_name, data.data)},
+        error: err => console.log(err)
+      })
     });
+  }
+
+  loadFacilities(municipality){
+    console.log(municipality);
+    this.http.get('libraries/facilities', {params:{'filter[municipality_code]':municipality, 'per_page': 'all'}}).subscribe({
+      next: (data: any) => {
+        console.log(data)
+        this.facilities = data.data;
+      },
+      error: err => console.log(err)
+    })
   }
 
   showPrivacyModal(){
@@ -112,7 +159,13 @@ export class UserRegistrationComponent implements OnInit {
       photo_url: [''],
       password: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{6,}$')]],
       password_confirmation: ['', Validators.required],
-      privacy: [false, Validators.requiredTrue]
+      privacy: [false, Validators.requiredTrue],
+      facility_code: [''],
+      region: ['', Validators.required],
+      province: ['', Validators.required],
+      municipality: ['', Validators.required],
+      designation_code: ['', Validators.required],
+      employer_code: ['', Validators.required],
     });
 
     this.date = new Date().toISOString().slice(0,10);
