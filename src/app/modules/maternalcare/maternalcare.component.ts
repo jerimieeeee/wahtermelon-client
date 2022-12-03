8bcdecdbe697673916cf0e3f2a40deb15180fdae
Mark Santos <christian.santos37@gmail.com>
2022-12-03 16:53:37 +0800

    load latest visit if open

--- src/app/modules/maternalcare/maternalcare.component.ts
+++ src/app/modules/maternalcare/maternalcare.component.ts
@@ -77,18 +77,21 @@ export class MaternalcareComponent implements OnInit {
 
   mcrID(type: any, id: any) {
     if (id) {
-      this.http.get('maternal-care/mc-records?type=' + type + '&patient_id=' + id).subscribe({
+      let params = {
+        type: type,
+        patient_id: id
+      }
+
+      this.http.get('maternal-care/mc-records', {params}).subscribe({
         next: (data: any) => {
-          // console.log(data.data[0]);
-          if (data.data.length == 0) {
-            this.patient_mc_list = ''
-          }
-          else {
-            console.log(data.data[0]);
+          console.log(data.data);
+          if (data.data.length > 0) {
             this.patient_mc_list = data.data;
+
+            if(!this.patient_mc_list[0].post_registration || !this.patient_mc_list[0].post_registration.end_pregnancy) {
+              this.openMCR(this.patient_mc_list[0].id);
+            }
           }
-
-
         },
         error: err => console.log(err),
       });
