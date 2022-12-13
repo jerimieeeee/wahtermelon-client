d5f2e6fe29be66303d7849739f4f76a5e2c897d3
Mark Santos <christian.santos37@gmail.com>
2022-12-13 12:10:40 +0800

    show pe on visit details in itr:

--- src/app/modules/patient-itr/components/visit-details/visit-details.component.ts
+++ src/app/modules/patient-itr/components/visit-details/visit-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,11 @@ import { Router } from '@angular/router';
   templateUrl: './visit-details.component.html',
   styleUrls: ['./visit-details.component.scss']
 })
-export class VisitDetailsComponent implements OnInit {
+export class VisitDetailsComponent implements OnChanges {
   @Input() latest_vitals;
   @Input() selected_visit;
+
+  physical_exam = [];
 
   openVisit(details: any){
     /* console.log(details);
@@ -16,11 +18,26 @@ export class VisitDetailsComponent implements OnInit {
     this.router.navigate(['/'+details.pt_group, {id:details.patient.id, consult_id: details.id}])
   }
 
+  groupPE(pe){
+    Object.entries(pe).forEach(([key, value], index) => {
+      let val: any = value;
+      if(!this.physical_exam[val.lib_physical_exam.category_id.toLowerCase()]) this.physical_exam[val.lib_physical_exam.category_id.toLowerCase()] = [];
+      this.physical_exam[val.lib_physical_exam.category_id.toLowerCase()].push(val.lib_physical_exam);
+    });
+
+    console.log(this.physical_exam)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.selected_visit) {
+      if(Object.keys(this.selected_visit.consult_notes.physical_exam).length > 0) {
+        this.groupPE(this.selected_visit.consult_notes.physical_exam);
+      }
+    }
+  }
+
   constructor(
     private router: Router
   ) { }
-
-  ngOnInit(): void {
-  }
 
 }
