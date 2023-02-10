691eb017001a614876bfcee922d9271a8ffdc834
Mark Santos <christian.santos37@gmail.com>
2023-02-10 09:25:37 +0800

    complete childcare summary on patient itr

--- src/app/modules/patient-itr/components/visit-details/group/cc/cc.component.ts
+++ src/app/modules/patient-itr/components/visit-details/group/cc/cc.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
@@ -9,13 +10,48 @@ import { HttpService } from 'app/shared/services/http.service';
 export class CcComponent implements OnInit {
   @Input() selected_visit;
 
+  faCircleNotch = faCircleNotch;
+
+  ccdev_record: any;
+  ccdev_breastfed: any;
+  ccdev_services: any;
+  show_form: boolean = false;
+
   loadData(){
-    this.http.get('child-care/cc-records/'+this.selected_visit.patient.id).subscribe({
+    this.fetchCc();
+  }
+
+  fetchCc(){
+    this.http.get('child-care/cc-records/'+this.selected_visit.patient.id)
+    .subscribe({
       next: (data: any) => {
-        console.log(data)
+        this.ccdev_record = data.data; console.log(data.data)
+
+        this.fetchBreastfed();
       },
       error: err => console.log(err)
     });
+  }
+
+  fetchBreastfed(){
+    this.http.get('child-care/cc-breastfed/'+this.selected_visit.patient.id)
+    .subscribe({
+      next: (data: any) => {
+        this.ccdev_breastfed = data.data; console.log(data.data);
+        this.fetchServices();
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  fetchServices(){
+    this.http.get('child-care/cc-services', {params:{patient_id: this.selected_visit.patient.id, sort:'service_id'}}).subscribe({
+      next: (data: any) => {
+        this.ccdev_services = data.data; console.log(data.data);
+        this.show_form = true;
+      },
+      error: err => console.log(err)
+    })
   }
 
   constructor(
