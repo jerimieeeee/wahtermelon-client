2438e747b79627246c6a270c6c3ae2317cb4ea41
Mark Santos <christian.santos37@gmail.com>
2023-03-02 11:36:30 +0800

    use delete-item component for deleting labs

--- src/app/modules/lab/lab.module.ts
+++ src/app/modules/lab/lab.module.ts
@@ -8,6 +8,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LabFormComponent } from './components/lab-form/lab-form.component';
 import { LabDeleteComponent } from './components/lab-delete/lab-delete.component';
 import { FormsModule } from '@angular/forms';
+import { DeleteItemComponent } from 'app/shared/shared-modals/delete-item/delete-item.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { FormsModule } from '@angular/forms';
     LabRoutingModule,
     SharedComponentsModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    DeleteItemComponent
   ]
 })
 export class LabModule { }
