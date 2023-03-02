9754144e51767f2b876ec0d05cf29cf5c6fccc62
Mark Santos <christian.santos37@gmail.com>
2023-03-02 11:52:07 +0800

    remove sharedmodule

--- src/app/modules/lab/lab.module.ts
+++ src/app/modules/lab/lab.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { LabRoutingModule } from './lab-routing.module';
 import { LabComponent } from './lab.component';
-import { SharedComponentsModule } from 'app/shared/shared-components/shared-components.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LabFormComponent } from './components/lab-form/lab-form.component';
 import { LabDeleteComponent } from './components/lab-delete/lab-delete.component';
@@ -20,7 +19,6 @@ import { DeleteItemComponent } from 'app/shared/shared-modals/delete-item/delete
   imports: [
     CommonModule,
     LabRoutingModule,
-    SharedComponentsModule,
     FontAwesomeModule,
     FormsModule,
     DeleteItemComponent
