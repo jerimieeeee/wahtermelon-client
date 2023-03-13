e6e39249a9b3e8a331878ff97732a6739a9a887e
Mark Santos <christian.santos37@gmail.com>
2023-03-13 16:49:06 +0800

    remove consultation, lab and prescription

--- src/app/modules/patient-info/modals/module-modal/module-modal.component.ts
+++ src/app/modules/patient-info/modals/module-modal/module-modal.component.ts
@@ -66,9 +66,9 @@ export class ModuleModalComponent implements OnInit {
         }, */
       }
     },
-    'Others': {
+    /* 'Others': {
       modules: {
-        /* cn: {
+        cn: {
           name: 'Laboratory',
           location: 'consultation',
           group: '',
@@ -79,7 +79,7 @@ export class ModuleModalComponent implements OnInit {
           location: 'consultation',
           group: '',
           consult_active: false
-        }, */
+        },
         tb: {
           name: 'Tuberculosis',
           location: 'tb',
@@ -93,12 +93,12 @@ export class ModuleModalComponent implements OnInit {
           consult_active: false
         },
       }
-    }
+    } */
   };
 
   itr = { name: 'Patient ITR', location: 'itr', group: '', consult_active: false };
   lab = { name: 'Laboratory', location: 'lab', group: '', consult_active: false };
-  cn = { name: 'Consultation', location: 'cn', group: 'cn', consult_active: false};
+  // cn = { name: 'Consultation', location: 'cn', group: 'cn', consult_active: false};
   mc = { name: 'Maternal Care', location: 'mc', group: 'mc', consult_active: false };
   cc = { name: 'Child Care', location: 'cc', group: 'cc', consult_active: false };
 
@@ -220,7 +220,7 @@ export class ModuleModalComponent implements OnInit {
   checkOpenConsult(consults){
     Object.entries(consults).forEach(([keys, values], indexes) => {
       let vals: any= values;
-      if(vals.pt_group === 'cn') this.cn.consult_active = true;
+      // if(vals.pt_group === 'cn') this.cn.consult_active = true;
 
       Object.entries(this.list_modules).forEach(([key, value], index) => {
         Object.entries(value.modules).forEach(([k, v], i) => {
