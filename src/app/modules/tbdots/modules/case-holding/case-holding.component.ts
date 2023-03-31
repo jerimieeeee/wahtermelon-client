a9bb4cef9c975b9c4baea76e499f47904d1b28a3
Mark Santos <christian.santos37@gmail.com>
2023-03-31 16:56:14 +0800

    update html template and controller

--- src/app/modules/tbdots/modules/case-holding/case-holding.component.ts
+++ src/app/modules/tbdots/modules/case-holding/case-holding.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faAdd, faSave } from '@fortawesome/free-solid-svg-icons';
+import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
   selector: 'app-case-holding',
   templateUrl: './case-holding.component.html',
   styleUrls: ['./case-holding.component.scss']
 })
-export class CaseHoldingComponent {
+export class CaseHoldingComponent implements OnInit {
+  faSave = faSave;
+  faAdd = faAdd;
 
+  caseHoldingForm: FormGroup = new FormGroup({
+    registration_date: new FormControl<string| null>(''),
+    case_number: new FormControl<string| null>(''),
+    enrolled_as: new FormControl<string| null>(''),
+    treatment_regimen: new FormControl<string| null>(''),
+    bacteriological_status: new FormControl<string| null>(''),
+    anatomical_site: new FormControl<string| null>(''),
+    eptb_site: new FormControl<string| null>(''),
+    specific_site: new FormControl<string| null>(''),
+    ipt_flag: new FormControl<string| null>(''),
+    drugresistant_flag: new FormControl<boolean| null>(false),
+    treatment_start: new FormControl<string| null>(''),
+    treatment_end: new FormControl<string| null>(''),
+    continuation_start: new FormControl<string| null>(''),
+    pict_date: new FormControl<string| null>(''),
+    transfer_flag: new FormControl<boolean| null>(false),
+  });
+
+  saveTreatment(){
+    console.log(this.caseHoldingForm.value)
+  }
+
+  loadLibraries(){
+    this.http.get('tbdots/tb-libraries-caseholding').subscribe({
+      next: (data: any) => {
+        console.log(data)
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadLibraries();
+  }
 }
--- src/app/modules/tbdots/modules/casefindings/casefindings.component.ts
+++ src/app/modules/tbdots/modules/casefindings/casefindings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faAdd, faSave } from '@fortawesome/free-solid-svg-icons';
 import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
@@ -7,20 +9,76 @@ import { HttpService } from 'app/shared/services/http.service';
   styleUrls: ['./casefindings.component.scss']
 })
 export class CasefindingsComponent implements OnInit {
-  delivery_method: any = [];
+  faSave = faSave;
+  faAdd = faAdd;
+
+  previousTreatmentForm: FormGroup = new FormGroup({
+    previous_treatment: new FormControl<string| null>(''),
+    previous_treatment_date: new FormControl<string| null>(''),
+  });
+
+  casefindingForm: FormGroup = new FormGroup({
+    patient_source: new FormControl<string| null>(''),
+    reg_group: new FormControl<string| null>(''),
+    consult_date: new FormControl<string| null>(''),
+    previous_tb_treatment: new FormControl<string| null>(''),
+    tb_exposure: new FormControl<string| null>(''),
+    presumptive_drtb: new FormControl<string| null>(''),
+    symptom: new FormGroup({
+      bcpain: new FormControl<boolean| false>(false),
+      cough: new FormControl<boolean| false>(false),
+      drest: new FormControl<boolean| false>(false),
+      dexertion: new FormControl<boolean| false>(false),
+      fever: new FormControl<boolean| false>(false),
+      hemoptysis: new FormControl<boolean| false>(false),
+      nsweats: new FormControl<boolean| false>(false),
+      pedema: new FormControl<boolean| false>(false),
+      wloss: new FormControl<boolean| false>(false),
+    }),
+    symptom_remarks: new FormControl<string| null>(''),
+    risk_factor: new FormGroup({
+      C: new FormControl<boolean| false>(false),
+      H: new FormControl<boolean| false>(false),
+      NC: new FormControl<boolean| false>(false),
+    })
+  });
 
   createForm() {
 
   }
 
-  onSubmit(data) {
-
+  savePreviousTreatment(){
+    console.log(this.previousTreatmentForm.value);
   }
+
+  saveCase() {
+    console.log(this.casefindingForm.value);
+  }
+
+  answers_yn: any = [];
+  patient_sources: any = [];
+  pe_answers: any = [];
+  pes: any = [];
+  previous_tb_treatments: any = [];
+  reg_groups: any = [];
+  risk_factors: any = [];
+  symptoms: any = [];
+  treatment_outcomes: any = [];
 
   loadLibraries(){
     this.http.get('tbdots/tb-libraries').subscribe({
       next: (data: any) => {
         console.log(data);
+        this.answers_yn = data.tb_answers_yn
+        this.patient_sources = data.tb_patient_sources;
+        this.pe_answers = data.tb_pe_answers;
+        this.pes = data.tb_pes;
+        this.previous_tb_treatments = data.tb_previous_tb_treatments;
+        this.reg_groups = data.tb_reg_groups;
+        this.risk_factors = data.tb_risk_factors;
+        this.symptoms = data.tb_symptoms;
+        this.treatment_outcomes = data.tb_treatment_outcomes;
+
       },
       error: err => console.log(err)
     });
