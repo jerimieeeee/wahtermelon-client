c60154a730c0c2022a645ab1d3169848d7ab05c1
Mark Santos <christian.santos37@gmail.com>
2023-05-17 18:33:41 +0800

    add modal for case outcome

---
+++ src/app/modules/gbv/modals/case-outcome/case-outcome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-outcome',
+  templateUrl: './case-outcome.component.html',
+  styleUrls: ['./case-outcome.component.scss']
+})
+export class CaseOutcomeComponent {
+
+}
