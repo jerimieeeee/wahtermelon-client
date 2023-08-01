4e68a3d04696ab35403373211f9b3b10b2999dd1
Mark Santos <christian.santos37@gmail.com>
2023-08-01 15:43:31 +0800

    update navigation for ab

--- src/app/modules/animalbite/animalbite.component.ts
+++ src/app/modules/animalbite/animalbite.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faCircleNotch, faDoorClosed, faPersonWalking } from '@fortawesome/free-solid-svg-icons';
+import { HttpService } from 'app/shared/services/http.service';
 
 @Component({
   selector: 'app-animalbite',
   templateUrl: './animalbite.component.html',
   styleUrls: ['./animalbite.component.scss']
 })
-export class AnimalbiteComponent {
+export class AnimalbiteComponent implements OnInit{
+  patient_id: any;
+  consult_id: any;
 
+  faPersonWalking = faPersonWalking;
+  faDoorClosed = faDoorClosed;
+  faCircleNotch = faCircleNotch;
+
+  pages: number = 1;
+  module: number = 1;
+  show_end: boolean = false;
+  fetching_history: boolean = true;
+
+  consult_details: any;
+
+  patient_ab_history: any = [];
+  modals: any = [];
+  selected_ab_consult: any;
+  user_facility: string;
+
+  max_date = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+
+  openTbConsult(data) {
+    this.selected_ab_consult = data;
+    if(data.treatment_done === 1) this.consult_details = null;
+    this.pages = 2;
+  }
+
+  getAbHistory(){
+    this.selected_ab_consult = null;
+    this.fetching_history = true;
+    let params = {
+      patient_id: this.patient_id,
+      per_page: 'all'
+    };
+
+    this.http.get('tbdots/patient-tb', {params}).subscribe({
+      next: (data: any) => {
+        this.patient_ab_history = data.data;
+        if(this.patient_ab_history[0] && this.patient_ab_history[0].treatment_done === 0) this.selected_ab_consult = data.data[0];
+        console.log(this.selected_ab_consult)
+        this.fetching_history = false;
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  switchPage(page) {
+    if(page === 1) this.getAbHistory();
+    this.pages = page;
+  }
+
+  switchTab(tab) {
+    this.module = tab;
+  }
+
+  toggleModal(modal_name) {
+    this.modals[modal_name] = !this.modals[modal_name];
+  }
+
+  loadConsult(){
+    let params = {
+      id: this.consult_id,
+      pt_group: 'ab'
+    };
+
+    this.http.get('consultation/records', {params}).subscribe({
+      next: (data: any) => {
+        this.consult_details = data.data[0];
+        this.getAbHistory();
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  constructor(
+    private http: HttpService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.user_facility = this.http.getUserFacility();
+    this.patient_id = this.route.snapshot.paramMap.get('id');
+    this.consult_id = this.route.snapshot.paramMap.get('consult_id');
+
+    this.loadConsult();
+  }
 }
--- src/app/modules/animalbite/animalbite.module.ts
+++ src/app/modules/animalbite/animalbite.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AnimalbiteRoutingModule } from './animalbite-routing.module';
 import { AnimalbiteComponent } from './animalbite.component';
+import { EndVisitComponent } from 'app/shared/shared-modals/end-visit/end-visit.component';
+import { SharedComponentsModule } from 'app/shared/shared-components/shared-components.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { AnimalbiteComponent } from './animalbite.component';
   ],
   imports: [
     CommonModule,
-    AnimalbiteRoutingModule
+    AnimalbiteRoutingModule,
+    EndVisitComponent,
+    SharedComponentsModule,
+    FontAwesomeModule
   ]
 })
 export class AnimalbiteModule { }
