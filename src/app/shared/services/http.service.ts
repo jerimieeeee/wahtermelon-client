40f4f1a0f1f909eeb790dca69e27a1f759c8f2d0
Mark Santos <christian.santos37@gmail.com>
2023-08-17 10:00:22 +0800

    udpate api route

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -9,8 +9,8 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class HttpService {
-  // baseUrl = 'https://apiwahtermelon-staging.wah.ph/api/v1/'; //staging-api
-  baseUrl = 'https://training-api.wah.ph/api/v1/'; //staging-api
+  baseUrl = 'https://apiwahtermelon-staging.wah.ph/api/v1/'; //staging-api
+  // baseUrl = 'https://training-api.wah.ph/api/v1/'; //staging-api
 
   // baseUrl = 'http://127.0.0.1:8000/api/v1/'; //local api
   constructor(
