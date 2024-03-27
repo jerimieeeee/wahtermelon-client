84bfae162fa77797928a23832b8c86c8e587e74d
Jerimie Garcia <je@Jerimie.local>
2024-03-27 14:21:54 +0800

    finish m1 fp report

--- src/app/modules/reports/report/fhsis2018-fp/fhsis2018-fp.component.ts
+++ src/app/modules/reports/report/fhsis2018-fp/fhsis2018-fp.component.ts
@@ -52,6 +52,111 @@ export class Fhsis2018FpComponent {
     }
   }
 
+  cu_beginning_month_total = {
+    current_user_beginning_month_10_to_14: 0,
+    current_user_beginning_month_15_to_19: 0,
+    current_user_beginning_month_20_to_49: 0,
+  }
+
+  countCUBeginningMonthTotal(indicator_name, value) {
+    this.cu_beginning_month_total[indicator_name] += value;
+  }
+
+  na_previous_month_total = {
+    new_acceptor_previous_month_10_to_14: 0,
+    new_acceptor_previous_month_15_to_19: 0,
+    new_acceptor_previous_month_20_to_49: 0,
+  }
+
+  countNAPreviousMonthTotal(indicator_name, value) {
+    this.na_previous_month_total[indicator_name] += value;
+  }
+
+  fp_total_users = {
+    // Current User (Beginning Month)
+    current_user_beginning_month_10_to_14: 0,
+    current_user_beginning_month_15_to_19: 0,
+    current_user_beginning_month_20_to_49: 0,
+
+    //New Acceptors (Previous Month)
+    new_acceptor_previous_month_10_to_14: 0,
+    new_acceptor_previous_month_15_to_19: 0,
+    new_acceptor_previous_month_20_to_49: 0,
+
+    //Other Acceptors (Previous Month)
+    other_acceptor_present_month_10_to_14: 0,
+    other_acceptor_present_month_15_to_19: 0,
+    other_acceptor_present_month_20_to_49: 0,
+
+    //Dropout (Present Month)
+    dropout_present_month_10_to_14: 0,
+    dropout_present_month_15_to_19: 0,
+    dropout_present_month_20_to_49: 0,
+
+    //Current User (End of Month)
+    cu_end_month_10_to_14: 0,
+    cu_end_month_15_to_19: 0,
+    cu_end_month_20_to_49: 0,
+
+    //New Acceptors (Present Month)
+    new_acceptor_present_month_10_to_14: 0,
+    new_acceptor_present_month_15_to_19: 0,
+    new_acceptor_present_month_20_to_49: 0,
+  }
+
+  countFpTotalUsers(indicator_name, value) {
+    this.fp_total_users[indicator_name] += value;
+  }
+
+  total_count = {
+    // Current User (Beginning Month)
+    cubm_10_14: 0,
+    cubm_15_19: 0,
+    cubm_20_49: 0,
+    // New Acceptor (Previous Month)
+    napr_10_14: 0,
+    napr_15_19: 0,
+    napr_20_49: 0,
+    // Other Acceptor (Present Month)
+    oapm_10_14: 0,
+    oapm_15_19: 0,
+    oapm_20_49: 0,
+    //Dropout (Present Month)
+    dopm_10_14: 0,
+    dopm_15_19: 0,
+    dopm_20_49: 0,
+    //Current User (End Month)
+    cuem_10_14: 0,
+    cuem_15_19: 0,
+    cuem_20_49: 0,
+    //New Acceptor (Present Month)
+    napm_10_14: 0,
+    napm_15_19: 0,
+    napm_20_49: 0,
+  }
+
+  method_list = [
+    {method_id: 'FSTRBTL', desc: 'a. BTL - Total'},
+    {method_id: 'MSV', desc: 'b. NSV - Total'},
+    {method_id: 'CONDOM', desc: 'c. Condom - Total'},
+    // {method_id: 'IUD_total', desc: 'd. IUD (I & PP)- Total'},
+    {method_id: 'IUD', desc: 'd.1. IUD-I - Total'},
+    {method_id: 'IUDPP', desc: 'd.2. IUD-PP - Total'},
+    // {method_id: 'PILLS_total', desc: 'e. Pills (POP & COC) - Total'},
+    {method_id: 'PILLSPOP', desc: 'e.1. Pills-POP - Total'},
+    {method_id: 'PILLS', desc: 'e.2. Pills-COC - Total'},
+    {method_id: 'DMPA', desc: 'f. Injectables (DMPA/CIC) - Total'},
+    {method_id: 'IMPLANT', desc: 'g. Implant - Total'},
+    {method_id: 'NFPCM', desc: 'h. NFP-CMM – Total'},
+    {method_id: 'NFPBBT', desc: 'i. NFP-BBT – Total'},
+    {method_id: 'NFPSTM', desc: 'j. NFP-STM – Total'},
+    {method_id: 'NFPSDM', desc: 'k. NFP-SDM - Total'},
+    {method_id: 'NFPLAM', desc: 'l. NFP-LAM - Total'},
+  ]
+
+  countTotalSummary(indicator_name, value) {
+    this.total_count[indicator_name] += value;
+  }
   exportX() {
     this.exportAsService.save(this.exportAsExcel, 'Family Planning M1').subscribe(() => {
       // save started
@@ -86,6 +191,11 @@ export class Fhsis2018FpComponent {
     // console.log(typeof name_list)
     this.name_list = list;
     this.openList = !this.openList;
+  }
+
+  countTotal(var_name: string) {
+    this[var_name] += 1;
+    // console.log(this[var_name]);
   }
 
   convertDate(){
