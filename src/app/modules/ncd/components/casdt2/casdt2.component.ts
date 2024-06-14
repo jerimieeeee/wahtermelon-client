804ac16d46a8aa457bcd3171da63c9fb405826fb
Jerimie Garcia <je@Jerimie.local>
2024-06-14 16:52:47 +0800

    added feedback report

--- src/app/modules/ncd/components/casdt2/casdt2.component.ts
+++ src/app/modules/ncd/components/casdt2/casdt2.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { faSearch,faBalanceScale,faPlus,faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import {questionnaireForm} from "../questionnaire/forms";
+import {casdtForm} from "../casdt/form";
+import {HttpService} from "../../../../shared/services/http.service";
+import {FormBuilder} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-casdt2',
@@ -7,46 +13,98 @@ import { faSearch,faBalanceScale,faPlus,faInfoCircle } from '@fortawesome/free-s
   styleUrls: ['./casdt2.component.scss']
 })
 export class Casdt2Component implements OnInit {
+  @Input() patient_id;
+  @Input() consult_details;
 
   faInfoCircle = faInfoCircle;
 
   eye = [
-    {code: 'blurred', desc: 'Blurred (Malabo, maulap o mausok)'},
-    {code: 'floaters', desc: 'Floaters (May lumulutang)'},
-    {code: 'tearing', desc: 'Tearing (Pagluluha)'},
-    {code: 'pain', desc: 'Pain (Mahapdi, masakit o mabigat sa pakiramdam)'},
-    {code: 'redness', desc: 'Redness (Namumula)'},
-    {code: 'glare', desc: 'Glare (Nasisilaw)'},
-    {code: 'discharge', desc: 'Discharge (May muta)'},
-    {code: 'photopsia', desc: 'Photopsia (May kumikislap)'},
-    {code: 'itchiness', desc: 'Itchiness (Makati)'}
+    {code: 'BLUR', desc: 'Blurred (Malabo, maupal o mausok)'},
+    {code: 'FLOAT', desc: 'Floaters (May lumulutang)'},
+    {code: 'TEAR', desc: 'Tearing (Pagluluha)'},
+    {code: 'PAIN', desc: 'Pain (Mahapdi, masakit o mabigat sa pakiramdam)'},
+    {code: 'RED', desc: 'Redness (Namumula)'},
+    {code: 'GLARE', desc: 'Glare (Nasisilaw)'},
+    {code: 'DISCH', desc: 'Discharge (May muta)'},
+    {code: 'PHOTOP', desc: 'Photopsia (May kumikislap)'},
+    {code: 'ITCH', desc: 'Itchiness (Makati)'}
   ];
 
   opthal = [
-    {code: 'a', desc: 'Eye Injury'},
-    {code: 'b', desc: 'Foreign Body'}
+    {code: 'INJURY', desc: 'Eye Injury'},
+    {code: 'FOREIGN', desc: 'Foreign Body'}
   ];
 
-
   vision_eye = [
-    {code: 'right', desc: 'Right Eye'},
-    {code: 'left', desc: 'Left Eye'},
-    {code: 'both', desc: 'Both Eye'},
-    {code: 'none', desc: 'None'}
+    {code: 'RIGHT', desc: 'Right Eye'},
+    {code: 'LEFT', desc: 'Left Eye'},
+    {code: 'BOTH', desc: 'Both Eye'},
+    {code: 'NONE', desc: 'None'}
   ];
 
   refer = [
-    {code: 'improve', desc: 'If VA is 20/40 to 20/100 but IMPROVES WITH PINHOLE: Refer to OPTOMETRIST'},
-    {code: 'not', desc: 'If VA is 20/40 to 20/100 but DOES NOT IMPROVE WITH PINHOLE: Refer to OPHTHALMOLOGIST'},
-    {code: 'worse', desc: 'If VA is 20/200 or worse: Refer to OPHTHALMOLOGIST'}
+    {code: 'IMPROVE', desc: 'If VA is 20/40 to 20/100 but IMPROVES WITH PINHOLE: Refer to OPTOMETRIST'},
+    {code: 'NOTIMPROVE', desc: 'If VA is 20/40 to 20/100 but DOES NOT IMPROVE WITH PINHOLE: Refer to OPHTHALMOLOGIST'},
+    {code: 'WORSE', desc: 'If VA is 20/200 or worse: Refer to OPHTHALMOLOGIST'}
   ];
 
+  constructor(
+    private http: HttpService,
+    private toastr: ToastrService
+  ) { }
 
+  is_saving: boolean = false;
+  show_form: boolean = false;
 
+  eye_complaints: any
+  eye_refer: any
+  eye_vision_screen: any
+  eye_refer_prof: any
 
-  constructor() { }
+  lodFormLibraries() {
+    const getEyeComplaints = this.http.get('libraries/ncd-eye-complaint');
+    const getEyeRefer = this.http.get('libraries/ncd-eye-refer');
+    const getEyeVisionScreen = this.http.get('libraries/ncd-eye-vision-screen');
+    const getEyeReferProf = this.http.get('libraries/ncd-eye-refer-prof');
 
-  ngOnInit(): void {
+    forkJoin([
+      getEyeComplaints,
+      getEyeRefer,
+      getEyeVisionScreen,
+      getEyeReferProf
+    ]).subscribe({
+      next: ([dataEyeComplaints, dataEyeRefer, dataEyeVisionScreen, dataEyeReferProf]: any) => {
+        this.eye_complaints = dataEyeComplaints.data;
+        this.eye_refer = dataEyeRefer.data;
+        this.eye_vision_screen = dataEyeVisionScreen.data;
+        this.eye_refer_prof = dataEyeReferProf.data;
+
+        // this.createForm();
+      },
+      error: err => console.log(err)
+    })
   }
 
+  onSubmit() {
+    this.is_saving = true;
+
+    if(this.casdt2Form.valid){
+      this.http.post('non-communicable-disease/risk-questionnaire', this.casdt2Form.value).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.toastr.success('Recorded successfully!','Questionnaire');
+          this.is_saving = false;
+        },
+        error: err => console.log(err)
+      })
+    }
+  }
+
+  ngOnInit(): void {
+    // console.log(this.patient_id, 'amen')
+    console.log(this.consult_details, 'amen')
+    this.lodFormLibraries();
+  }
+
+  protected readonly casdt2Form = casdtForm;
 }
