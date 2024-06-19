8dc7e9ed6d531eed3df5ab615cb47497b3d36157
Jerimie Garcia <je@Jerimie.local>
2024-06-19 08:45:59 +0800

    commit

--- src/app/modules/ncd/components/casdt2/casdt2.component.ts
+++ src/app/modules/ncd/components/casdt2/casdt2.component.ts
@@ -1,11 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { faSearch,faBalanceScale,faPlus,faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faBalanceScale, faPlus, faInfoCircle, faSpinner} from '@fortawesome/free-solid-svg-icons';
 import {questionnaireForm} from "../questionnaire/forms";
 import {casdtForm} from "../casdt/form";
 import {HttpService} from "../../../../shared/services/http.service";
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ToastrService} from "ngx-toastr";
 import {forkJoin} from "rxjs";
+import {oralCOnditionForm} from "../../../dental/components/condition/oralConditionForm";
+import {casdt2Form} from "./casdt2form";
+import {faSave} from "@fortawesome/free-regular-svg-icons";
+import {riskAssessForm} from "../risk-assessment/forms";
 
 @Component({
   selector: 'app-casdt2',
@@ -16,44 +20,16 @@ export class Casdt2Component implements OnInit {
   @Input() patient_id;
   @Input() consult_details;
 
+  casdt2Forms:FormGroup=casdt2Form();
+
   faInfoCircle = faInfoCircle;
-
-  eye = [
-    {code: 'BLUR', desc: 'Blurred (Malabo, maupal o mausok)'},
-    {code: 'FLOAT', desc: 'Floaters (May lumulutang)'},
-    {code: 'TEAR', desc: 'Tearing (Pagluluha)'},
-    {code: 'PAIN', desc: 'Pain (Mahapdi, masakit o mabigat sa pakiramdam)'},
-    {code: 'RED', desc: 'Redness (Namumula)'},
-    {code: 'GLARE', desc: 'Glare (Nasisilaw)'},
-    {code: 'DISCH', desc: 'Discharge (May muta)'},
-    {code: 'PHOTOP', desc: 'Photopsia (May kumikislap)'},
-    {code: 'ITCH', desc: 'Itchiness (Makati)'}
-  ];
-
-  opthal = [
-    {code: 'INJURY', desc: 'Eye Injury'},
-    {code: 'FOREIGN', desc: 'Foreign Body'}
-  ];
-
-  vision_eye = [
-    {code: 'RIGHT', desc: 'Right Eye'},
-    {code: 'LEFT', desc: 'Left Eye'},
-    {code: 'BOTH', desc: 'Both Eye'},
-    {code: 'NONE', desc: 'None'}
-  ];
-
-  refer = [
-    {code: 'IMPROVE', desc: 'If VA is 20/40 to 20/100 but IMPROVES WITH PINHOLE: Refer to OPTOMETRIST'},
-    {code: 'NOTIMPROVE', desc: 'If VA is 20/40 to 20/100 but DOES NOT IMPROVE WITH PINHOLE: Refer to OPHTHALMOLOGIST'},
-    {code: 'WORSE', desc: 'If VA is 20/200 or worse: Refer to OPHTHALMOLOGIST'}
-  ];
 
   constructor(
     private http: HttpService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
   ) { }
 
-  is_saving: boolean = false;
   show_form: boolean = false;
 
   eye_complaints: any
@@ -78,33 +54,54 @@ export class Casdt2Component implements OnInit {
         this.eye_refer = dataEyeRefer.data;
         this.eye_vision_screen = dataEyeVisionScreen.data;
         this.eye_refer_prof = dataEyeReferProf.data;
+        this.show_form = true;
 
-        // this.createForm();
+        this.createForm();
       },
-      error: err => console.log(err)
+      error: err => { this.http.showError(err.error.message, 'Casdt2 Library'); }
     })
   }
 
+  createForm() {
+    this.casdt2Forms = this.formBuilder.nonNullable.group({
+      id: [''],
+      consult_ncd_risk_id: [this.consult_details.id, [Validators.required]],
+      patient_ncd_id: [this.consult_details.patient_ncd_id, [Validators.required]],
+      consult_id: [this.consult_details.consult_id, [Validators.required]],
+      patient_id: [this.consult_details.patient_id, [Validators.required]],
+      eye_complaint: [null, Validators.required],
+      eye_refer: [null, Validators.required],
+      unaided: [null, Validators.required],
+      pinhole: [null, Validators.required],
+      improved: [null, Validators.required],
+      aided: [null, Validators.required],
+      eye_refer_prof: [null, Validators.required],
+    });
+  }
+
+  is_saving: boolean = false;
   onSubmit() {
+    console.log(this.consult_details)
     this.is_saving = true;
 
     if(this.casdt2Form.valid){
-      this.http.post('non-communicable-disease/risk-questionnaire', this.casdt2Form.value).subscribe({
+      this.http.post('non-communicable-disease/risk-casdt-vision', this.casdt2Forms.value).subscribe({
         next: (data: any) => {
           console.log(data);
-          this.toastr.success('Recorded successfully!','Questionnaire');
+          this.toastr.success('Recorded successfully!','Casdt2');
           this.is_saving = false;
         },
-        error: err => console.log(err)
+        error: err => { this.http.showError(err.error.message, 'Casdt2'); }
       })
     }
   }
 
   ngOnInit(): void {
-    // console.log(this.patient_id, 'amen')
-    console.log(this.consult_details, 'amen')
     this.lodFormLibraries();
   }
 
   protected readonly casdt2Form = casdtForm;
+  protected readonly faSave = faSave;
+  protected readonly faSpinner = faSpinner;
+  protected readonly riskAssessForm = riskAssessForm;
 }
