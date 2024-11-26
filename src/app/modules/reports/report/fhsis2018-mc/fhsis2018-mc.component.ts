299534c676553a29cfcf5bbb83b5412c078a3b06
Mark Santos <christian.santos37@gmail.com>
2024-11-26 08:43:54 +0800

    "Refactored header component for FHSIS 2018 report, moved header logic to separate component, and updated template and styles accordingly."

--- src/app/modules/reports/report/fhsis2018-mc/fhsis2018-mc.component.ts
+++ src/app/modules/reports/report/fhsis2018-mc/fhsis2018-mc.component.ts
@@ -93,20 +93,20 @@ export class Fhsis2018McComponent implements OnChanges {
   }
 
   convertBrgy(){
-    this.brgy_result = this.selected_barangay?.map((code) => this.brgys.find((el) => el.code == code).name);
+    this.brgy_result = this.selectedBrgy?.map((code) => this.brgys.find((el) => el.code == code).name);
   }
 
-  label_value: {};
+  // label_value: {};
   ngOnChanges(): void {
     this.current_submit_flag = this.submit_flag;
     if(this.current_submit_flag){
       this.show_stats = false;
       this.stats = this.report_data;
-      this.brgys_info = this.brgys;
+      // this.brgys_info = this.brgys;
       this.pdf_exported = false;
-      this.label_value = this.dateHelper.getLabelValue(this.reportForm, this.report_data);
-      console.log()
-      if(this.selectedBrgy) this.convertBrgy();
+      // this.label_value = this.dateHelper.getLabelValue(this.reportForm, this.report_data);
+      // console.log()
+      // if(this.selectedBrgy) this.convertBrgy();
 
       this.show_stats = true;
     }
