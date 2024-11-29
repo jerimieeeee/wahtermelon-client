773014da781e39c8e01054e6b7b97d9f7bfd5793
vicballs <vinantonioph@gmail.com>
2024-11-29 11:19:58 +0800

    fix facility based stats and disable adolescent on health program modules

---
+++ src/app/modules/adolescent/modules/comprehensive-heeadsss/safety/safety.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-safety',
+  templateUrl: './safety.component.html',
+  styleUrl: './safety.component.scss'
+})
+export class SafetyComponent {
+
+}
---
+++ src/app/modules/adolescent/modules/comprehensive-heeadsss/suicide-depression/suicide-depression.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suicide-depression',
+  templateUrl: './suicide-depression.component.html',
+  styleUrl: './suicide-depression.component.scss'
+})
+export class SuicideDepressionComponent {
+
+}
