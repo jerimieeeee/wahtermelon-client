000ad4bff56c2baaa5f868605efb849a343aca12
vicballs <vinantonioph@gmail.com>
2025-01-30 13:18:24 +0800

    added referral on asrh

---
+++ src/app/modules/adolescent/modules/comprehensive-heeadsss/referral/referral.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-referral',
+  standalone: true,
+  imports: [],
+  templateUrl: './referral.component.html',
+  styleUrl: './referral.component.scss'
+})
+export class ReferralComponent {
+
+}
