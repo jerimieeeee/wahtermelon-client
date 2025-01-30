871ef518b53e086cccf5934b0643b74ca8bcd70f
vicballs <vinantonioph@gmail.com>
2025-01-30 13:28:59 +0800

    update referral

---
+++ src/app/modules/adolescent/modules/comprehensive-heeadsss/referral/modals/referral-modal/referral-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './referral-modal.component.html',
+  styleUrl: './referral-modal.component.scss'
+})
+export class ReferralModalComponent {
+
+}
