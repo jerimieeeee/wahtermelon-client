4226d12e52209c93c40f48d0c09fdbfd64d74336
Mark Santos <christian.santos37@gmail.com>
2025-04-30 14:32:13 +0800

    remove extra space

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,7 +5,6 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-
 @NgModule({
   declarations: [
     AdminComponent
