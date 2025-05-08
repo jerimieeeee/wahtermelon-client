fcf6182cc8454dc3423a5ba5367a07745a9462c2
vicballs <vinantonioph@gmail.com>
2025-05-08 08:58:30 +0800

    added asrh masterlist

---
+++ src/app/modules/reports/report/asrh-masterlist/asrh-masterlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asrh-masterlist',
+  imports: [],
+  templateUrl: './asrh-masterlist.component.html',
+  styleUrl: './asrh-masterlist.component.scss'
+})
+export class AsrhMasterlistComponent {
+
+}
